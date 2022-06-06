d9d8cf25db351ee891a17b3a435db49afb797d73
Keith Meyer <meyer.keith@gmail.com>
2022-06-06 00:01:28 -0400

    Clean up and use quasi-real logging.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import discord
 import os
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    logger.log(logging.INFO, 'We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
